1c05b97c3547c79e5c2d81398506018ab81601cb
Sweety Rawat <57385799+Sweetyrawat-star@users.noreply.github.com>
2024-03-13 17:41:15 +0530

    changes done in calculator

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,3 +1,4 @@
+/*
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -263,5 +264,191 @@ class _CalculatorState extends State<Calculator> {
       return white;
     }
 
+  }
+}*/
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+import 'package:nookienation_tictactoe_calc/Helper/color.dart';
+
+const operatorcolor = Color(0xff272727);
+const buttonColor = Color(0xff191919);
+const orangecolor = Color(0xffD9802E);
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  //Variables
+  double firstnumber = 0.0;
+  double secondnumber = 0.0;
+  var input = '';
+  var output = '';
+  var operation = '';
+  var hideInput = false;
+  var outputSize = 34.0;
+
+  onButtonClick(value) {
+    //if value is AC
+    if (value == 'AC') {
+      input = '';
+      output = '';
+    } else if (value == '<') {
+      if (input.isNotEmpty) {
+        input = input.substring(0, input.length - 1);
+      }
+    } else if (value == '=') {
+      if (input.isNotEmpty) {
+        var userInput = input;
+        userInput = input.replaceAll('X', '*');
+        Parser p = Parser();
+        Expression expression = p.parse(userInput);
+        ContextModel cm = ContextModel();
+        var finalValue = expression.evaluate(EvaluationType.REAL, cm);
+        output = finalValue.toString();
+        if (output.endsWith(".0")) {
+          output = output.substring(0, output.length - 2);
+        }
+        input = output;
+        hideInput = true;
+        outputSize = 52.0;
+      }
+    } else {
+      input = input + value;
+      hideInput = false;
+      outputSize = 34.0;
+    }
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Platform.isAndroid
+                ? IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+                : BackButton(
+              color: white,
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            //Input and output area
+            Expanded(
+                child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          hideInput ? '' : input,
+                          style: const TextStyle(
+                            fontSize: 48,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          output,
+                          style: TextStyle(
+                            fontSize: outputSize,
+                            color: Colors.white.withOpacity(0.7),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        )
+                      ],
+                    )
+                )
+            ),
+            Row(children: [
+              button(
+                  text: 'AC', buttonBGcolor: grey, tColor: white),
+              button(
+                  text: 'CE', buttonBGcolor: grey, tColor: white),
+              button(
+                  text: '%', buttonBGcolor: grey, tColor: white),
+              button(
+                  text: '÷', buttonBGcolor: grey, tColor: white),
+            ]),
+            Row(children: [
+              button(text: '7'),
+              button(text: '8'),
+              button(text: '9'),
+              button(
+                  text: 'X', buttonBGcolor: grey, tColor: white),
+            ]),
+            Row(children: [
+              button(text: '4'),
+              button(text: '5'),
+              button(text: '6'),
+              button(
+                  text: '-', buttonBGcolor: grey, tColor: white),
+            ]),
+            Row(children: [
+              button(text: '1'),
+              button(text: '2'),
+              button(text: '3'),
+              button(
+                  text: '+', buttonBGcolor: grey, tColor: white),
+            ]),
+            Row(children: [
+              button(
+                  text: '00'),
+              button(text: '0'),
+              button(text: '.'),
+              button(text: '=', buttonBGcolor: orangecolor),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget button({text, tColor = Colors.white, buttonBGcolor = buttonColor}) {
+    return Expanded(
+        child: Container(
+          margin: const EdgeInsets.all(8),
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)), backgroundColor: buttonBGcolor,
+                padding: const EdgeInsets.all(22),
+              ),
+              onPressed: () => onButtonClick(text),
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: tColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+          ),
+        )
+    );
   }
 }
